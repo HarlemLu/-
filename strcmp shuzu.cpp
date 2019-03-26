776fb5cbf018a71eda9a885ca9a4b885ec9d5bde
卢涛 <48885125+HarlemLu@users.noreply.github.com>
2019-03-26 20:21:22 +0800

    Add files via upload

---
+++ strcmp shuzu.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+
+int mycmp(const char*s1,const char*s2)
+{
+	int idx=0;
+	while(s1[idx]==s2[idx]&&s1[idx]!='\0'){
+		idx++;
+	}
+	return *s1-*s2;
+}
+
+int main(int argt,char const *argv[])
+{
+	char s1[]="abc";
+	char s2[]="bbc";
+	printf("%d\n",mycmp(s1,s2));
+	printf("%d\n",'a'-'A');
+	return 0;                           
+}
